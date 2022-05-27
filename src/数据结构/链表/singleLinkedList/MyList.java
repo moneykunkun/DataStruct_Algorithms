9aecc7c5768f0c430b4ec4375419c61d69e3f56c
35890 <35890226@qq.com>
2022-05-27 23:15:11 +0800

    新增单链表的相关方法

--- src/数据结构/链表/singleLinkedList/MyList.java
+++ src/数据结构/链表/singleLinkedList/MyList.java
@@ -4,10 +4,19 @@ package 数据结构.链表.singleLinkedList;
 public interface MyList {
         //添加元素
     void addFirst(int val);
-    //删除元素
-    int remove(int index);
+    //尾插
+    void addLast(int val);
+    //在任意索引位置插入元素
+    void addIndex(int index,int val);
     //通过索引获得元素
     int get(int index);
+    //修改指定索引位置的元素，返回修改前的元素
+    int set(int index,int newVal);
+    //判断链表中是否包含指定元素
+    boolean contains(int val);
+
+    //删除元素
+    int remove(int index);
     //获取链表的长度
     int size();
 }
--- src/数据结构/链表/singleLinkedList/SingleList.java
+++ src/数据结构/链表/singleLinkedList/SingleList.java
@@ -38,12 +38,106 @@ public class SingleList implements MyList{
         size++;
     }
 
+    //尾插
+    @Override
+    public void addLast(int val) {
+        addIndex(size,val);
+    }
+
+    /**
+     * 在任意索引位置插入元素
+     * @param index
+     * @param val
+     */
+    @Override
+    public void addIndex(int index, int val) {
+        //判断索引的合法性
+        if (index<0 || index >size){
+            System.out.println("index illegal");
+            return;
+        }
+        if (index==0){
+            addFirst(val);
+            return;
+        }
+        //插入元素
+        Node node =new Node(val);
+        //需要找到待插入元素的前驱,从头节点向后走index-1步
+        Node prev =head;
+        for (int i = 0; i < index-1; i++) {
+            prev=prev.next;
+        }
+        //此时，prev指向待插入元素的前期节点
+        node.next=prev.next;
+        prev.next=node;
+        size++;
+    }
+
+    //根据索引获取指定元素值
+    @Override
+    public int get(int index) {
+        if (checkRange(index)){
+            //index 合法
+            //从头节点开始遍历链表，走到index位置
+            Node node =head;
+            for (int i = 0; i < index; i++) {
+                node =node.next;
+            }
+            return node.val;
+        }else {
+            System.out.println("index illegal");
+            return -1;
+        }
+    }
+
+    //修改索引位置的元素
+    @Override
+    public int set(int index, int newVal) {
+        if (checkRange(index)){
+            Node node =head;
+            for (int i = 0; i < index; i++) {
+                node=node.next;
+            }
+            int oldVal =node.val;
+            node.val =newVal;
+        }else {
+            System.out.println("index illegal");
+            return -1;
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean contains(int val) {
+        for (Node temp=head; temp !=null;temp=temp.next){
+            if (temp.val==val){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //校验索引的合法性
+    private boolean checkRange(int index){
+        if (index<0 || index >=size){
+            return false;
+        }
+        return true;
+    }
     @Override
     public String toString() {
-        return "SingleList{" +
-                "head=" + head +
-                ", size=" + size +
-                '}';
+        String ret="";
+        //遍历链表
+        //先存储一下头节点
+        Node node =head;
+        while (node !=null){
+            ret += node.val;
+            ret+="->";
+            //继续向后遍历访问
+            node =node.next;
+        }
+        ret +="NULL";
+        return  ret;
     }
 
     @Override
@@ -51,13 +145,12 @@ public class SingleList implements MyList{
         return 0;
     }
 
-    @Override
-    public int get(int index) {
-        return 0;
-    }
+
 
     @Override
     public int size() {
         return 0;
     }
+
+
 }
