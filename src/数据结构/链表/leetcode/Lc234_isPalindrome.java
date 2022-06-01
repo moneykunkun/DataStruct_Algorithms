ba3f8c6a778afabc201a6c71ec440f1cd8293a07
35890 <35890226@qq.com>
2022-06-01 22:41:28 +0800

    更新

--- src/数据结构/链表/leetcode/Lc234_isPalindrome.java
+++ src/数据结构/链表/leetcode/Lc234_isPalindrome.java
@@ -1,6 +1,7 @@
 package 数据结构.链表.leetcode;
 
-//  给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false
+//  给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。
+//  如果是，返回 true ；否则，返回 false
 public class Lc234_isPalindrome {
     public boolean isPalindrome(ListNode head) {
     //找到中间节点
