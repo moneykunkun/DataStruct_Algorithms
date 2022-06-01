3bf1da4550b361d2cec939082c498c474904a3a1
35890 <35890226@qq.com>
2022-06-01 22:41:46 +0800

    更新

--- src/数据结构/链表/leetcode/Lc234_isPalindrome2.java
+++ src/数据结构/链表/leetcode/Lc234_isPalindrome2.java
@@ -2,7 +2,7 @@ package 数据结构.链表.leetcode;
 
 public class Lc234_isPalindrome2 {
     public boolean isPalindrome(ListNode head) {
-    //创建一个新链表，新链表是原链表的倒置
+        //创建一个新链表，新链表是原链表的倒置
         ListNode newHead= reverse(head);
         //同步遍历两个链表，发现有值不相等时，return false
         while (head != null){
@@ -14,7 +14,6 @@ public class Lc234_isPalindrome2 {
         }
         return true;
     }
-
     //反转链表
     public ListNode reverse(ListNode head){
         ListNode newHead =null;
