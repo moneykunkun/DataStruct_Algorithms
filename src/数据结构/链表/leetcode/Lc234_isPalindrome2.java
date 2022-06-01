208f7bd2a66b489e92a507802bff21f600c54054
35890 <35890226@qq.com>
2022-06-01 14:31:47 +0800

    新增 判断链表是否是回环链表

---
+++ src/数据结构/链表/leetcode/Lc234_isPalindrome2.java
@@ -0,0 +1,30 @@
+package 数据结构.链表.leetcode;
+
+public class Lc234_isPalindrome2 {
+    public boolean isPalindrome(ListNode head) {
+    //创建一个新链表，新链表是原链表的倒置
+        ListNode newHead= reverse(head);
+        //同步遍历两个链表，发现有值不相等时，return false
+        while (head != null){
+            if (head.val != newHead.val){
+                return false;
+            }
+            head =head.next;
+            newHead =newHead.next;
+        }
+        return true;
+    }
+
+    //反转链表
+    public ListNode reverse(ListNode head){
+        ListNode newHead =null;
+        while (head !=null){
+            ListNode node =new ListNode(head.val);
+            node.next =newHead;
+            newHead =node;
+            head =head.next;
+        }
+        return newHead;
+    }
+
+}
