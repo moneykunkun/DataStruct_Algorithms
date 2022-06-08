4178208a06c86355f0dc2afe80f73dfe6d5f2a24
35890 <35890226@qq.com>
2022-06-08 14:37:59 +0800

    新增 循环队列的实现 入队

--- src/数据结构/队列/queue/impl/LoopQueue.java
+++ src/数据结构/队列/queue/impl/LoopQueue.java
@@ -1,18 +1,23 @@
-package 数据结构.队列.queue;
+package 数据结构.队列.queue.impl;
+
+import 数据结构.队列.queue.Queue;
+
+import java.util.NoSuchElementException;
 
 /**
- * 循环队列
+ * 基于整形的循环队列
  */
-public class LoopQueue implements Queue<Integer>{
+public class LoopQueue implements Queue<Integer> {
 
     private Integer[] date;             //存储数据的数组
     private int head;
+    //tail 指向队列队尾元素的下一个位置
     private int tail;
     private int size;
 
-    public LoopQueue(int size){
-        //循环队列，需要一个额外的空间开辟一个空间
-        date =new Integer[size+1];
+    public LoopQueue(int n){
+        //循环队列，需要一个额外的空间开辟一个空间。判断队列是否已满
+        date =new Integer[n+1];
     }
     @Override
     public void offer(Integer val) {
@@ -27,7 +32,13 @@ public class LoopQueue implements Queue<Integer>{
 
     @Override
     public Integer poll() {
-        return null;
+        if (isFull()){
+            throw  new NoSuchElementException("queue is empty!");
+        }
+        Integer val =date[head];
+        head =(head+1)%date.length;             //继续向后移动
+        size--;
+        return val;
     }
 
     @Override
@@ -37,7 +48,7 @@ public class LoopQueue implements Queue<Integer>{
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return head ==tail;
     }
 
     /**
