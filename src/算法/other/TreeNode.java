32d9c693daddc898f13a4e8e72ec272e2b67a20a
35890 <35890226@qq.com>
2022-06-10 16:15:45 +0800

    新增 判断平衡二叉树的优化 记忆化搜索

---
+++ src/算法/other/TreeNode.java
@@ -0,0 +1,23 @@
+package 算法.other;
+
+/**
+ * LeetCode测试用例
+ */
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {
+          }
+
+          TreeNode(int val) {
+              this.val = val;
+          }
+
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
