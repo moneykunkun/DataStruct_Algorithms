66293d7a1d7d20bcc65646bd870b8d7b4a2cb7c3
35890 <35890226@qq.com>
2022-06-11 22:40:21 +0800

    新增 选择排序

--- src/算法/排序/SelectionSort.java
+++ src/算法/排序/SelectionSort.java
@@ -2,16 +2,22 @@ package 算法.排序;
 
 import java.util.Arrays;
 
+/**
+ * 选择排序
+ */
 public class SelectionSort {
     public static void selectionSort(int[] arr){
         if (arr==null || arr.length<2){
+            //只有一个元素或者没有元素 直接返回
             return;
         }
         for (int i=0;i<arr.length-1;i++){
             int minIndex=i;
             for (int j=i+1;j<arr.length;j++){
+                //最小元素的索引
                 minIndex =arr[j]<arr[minIndex] ?j:minIndex;
             }
+            //交换索引
             swap(arr,i,minIndex);
         }
     }
