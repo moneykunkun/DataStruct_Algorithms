9cc0969c88b5843c03048a8bfea386f293080fd5
35890 <35890226@qq.com>
2022-06-13 22:54:12 +0800

    新增 删除重复元素

---
+++ src/数据结构/链表/leetcode/Lc82_deleteDuplicates.java
@@ -0,0 +1,25 @@
+package 数据结构.链表.leetcode;
+
+
+
+public class Lc82_deleteDuplicates {
+    public ListNode deleteDuplicates(ListNode head) {
+        if ( head == null){
+            return null;
+        }
+        //定义一个虚拟头节点
+        ListNode dummyHead =new ListNode(-101);
+        ListNode p =dummyHead;
+        dummyHead.next =head;
+        while (p.next != null){
+            ListNode q= p.next;
+            while (q.next != null && q.next.val == q.val) q=q.next;
+            if (q == p.next){
+                p=p.next;
+            }else {
+                p.next =q.next;
+            }
+        }
+        return dummyHead.next;
+    }
+}
