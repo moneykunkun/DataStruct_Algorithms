43d282925103d35d28ed934169519041c335529d
35890 <35890226@qq.com>
2022-06-14 23:27:55 +0800

    新增 45. 跳跃游戏 II

--- src/算法/动态规划/Num45_jump.java
+++ src/算法/动态规划/Num45_jump.java
@@ -1,6 +1,12 @@
 package 算法.动态规划;
 
-public class Lc45_jump {
+/**
+ * 给你一个非负整数数组 nums ，你最初位于数组的第一个位置。
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+ * 假设你总是可以到达数组的最后一个位置。
+ */
+public class Num45_jump {
     public int jump(int[] nums) {
     int len = nums.length;
     int[] dp =new int[len];
