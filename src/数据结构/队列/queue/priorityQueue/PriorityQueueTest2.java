5440cbf8a19800ec026ae0d221e197f6e356c548
35890 <35890226@qq.com>
2022-06-18 23:58:04 +0800

    新增 jdk最小堆的测试

--- src/数据结构/队列/queue/priorityQueue/PriorityQueueTest2.java
+++ src/数据结构/队列/queue/priorityQueue/PriorityQueueTest2.java
@@ -1,10 +1,13 @@
 package 数据结构.队列.queue.priorityQueue;
 
-import 数据结构.队列.queue.Queue;
-
 import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
-public class PriorityQueueTest {
+/**
+ * JDK中优先级队列的最小堆实现
+ */
+public class PriorityQueueTest2 {
     public static void main(String[] args) {
         int[] data ={1,10,12,9,6,15,7,3,8,18};
         Queue<Integer> queue =new PriorityQueue();
