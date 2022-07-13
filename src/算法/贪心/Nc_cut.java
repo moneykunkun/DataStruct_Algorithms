84dea835777c856b3e111ccf854b62126a602db4
35890 <35890226@qq.com>
2022-07-13 21:27:30 +0800

    新增 链表

--- src/算法/贪心/Nc_cut.java
+++ src/算法/贪心/Nc_cut.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 //切割花布条
 //https://www.nowcoder.com/questionTerminal/1046cd038f7a4b04b2b77b415973de1c
 public class Nc_cut {
+
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         while (sc.hasNext()){
