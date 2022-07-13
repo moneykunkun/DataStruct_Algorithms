989714f2dd656fbba6e8a51ca8763f4efcf27cdf
35890 <35890226@qq.com>
2022-07-13 21:27:11 +0800

    新增 链表

--- src/数据结构/链表/leetcode/Offer22_getKthFromEnd.java
+++ src/数据结构/链表/leetcode/Offer22_getKthFromEnd.java
@@ -6,6 +6,7 @@ public class Offer22_getKthFromEnd {
     //要找到倒数第K个节点，单链表只能从前往后遍历，因此引入两个指针
     //先让快指针走K步，保证快慢指针的相对位置
     //当fast为空时，low即是所求的倒数第K个节点
+
     public ListNode getKthFromEnd(ListNode head, int k) {
         //快指针，从头开始
         ListNode fast= head;
