b5806a6b64c547c108b34e6ae135cb5c47fed484
35890 <35890226@qq.com>
2022-07-13 20:59:32 +0800

    新增 快速排序

--- src/算法/排序/QuickSortPlus.java
+++ src/算法/排序/QuickSortPlus.java
@@ -2,11 +2,33 @@ package 算法.排序;
 
 public class QuickSortPlus {
 
+
+    /**
+     * 分区函数，找到基准值
+     * @param arr
+     * @param left
+     * @param right
+     * @return
+     */
+    public static int partition(int[] arr,int left,int right){
+
+
+    }
     /**
      * 快速排序
      * @param arr
      */
-    public static void quickSort(int arr){
+    public static void quickSort(int[] arr,int left,int right){
 
-}
+        //找到基准值
+        if (left>=right) return;
+
+        //找到基准值
+        int pivot=partition(arr,left,right);
+
+        //递归基准值的左区间
+        quickSort(arr,left,pivot-1);
+        //递归基准值的右区间
+        quickSort(arr,pivot+1,right);
+        }
 }
