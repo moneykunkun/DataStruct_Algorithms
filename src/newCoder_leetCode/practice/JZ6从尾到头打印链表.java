4021759d5fa7df90cae573764fffd3c145ca87ca
35890 <35890226@qq.com>
2022-07-15 16:52:19 +0800

    新增 JZ6从尾到头打印链表

---
+++ src/newCoder_leetCode/practice/JZ6从尾到头打印链表.java
@@ -0,0 +1,35 @@
+package newCoder_leetCode.practice;
+
+import java.util.ArrayList;
+public class JZ6从尾到头打印链表 {
+
+        public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+            ArrayList<Integer> list= new ArrayList<>();
+            //反转链表
+            ListNode head = reverseList(listNode);
+            while(head !=null){
+                list.add(head.val);
+                head =head.next;
+            }
+            return list;
+
+        }
+        //反转链表
+        public static ListNode reverseList(ListNode head) {
+            if (head ==null || head.next ==null){
+                return head;
+            }
+            ListNode prev =null;
+            //当前需要处理的节点，需要反转
+            ListNode cur =head;
+            while (cur !=null){
+                //暂存一下需要处理的节点
+                ListNode next =cur.next;
+                cur.next =prev;
+                prev =cur;
+                cur =next;
+            }
+            return prev;
+        }
+    }
+
---
+++ src/newCoder_leetCode/practice/ListNode.java
@@ -0,0 +1,18 @@
+package newCoder_leetCode.practice;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
