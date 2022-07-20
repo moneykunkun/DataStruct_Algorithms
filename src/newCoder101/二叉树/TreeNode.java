a28c675a4780c1eaa2ecb0b285434ffb17ba9524
35890 <35890226@qq.com>
2022-07-20 09:44:39 +0800

    新增 TreeNode

---
+++ src/newCoder101/二叉树/TreeNode.java
@@ -0,0 +1,23 @@
+package newCoder101.二叉树;
+
+/**
+ * LeetCode测试用例
+ */
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {
+          }
+
+          TreeNode(int val) {
+              this.val = val;
+          }
+
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
