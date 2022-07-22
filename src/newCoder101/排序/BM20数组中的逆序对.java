0a3260c750185aa39c4524e7ca70c4740170ecac
35890 <35890226@qq.com>
2022-07-22 21:30:39 +0800

    新增 BM20数组中的逆序对

--- src/newCoder101/排序/BM20数组中的逆序对.java
+++ src/newCoder101/排序/BM20数组中的逆序对.java
@@ -7,7 +7,7 @@ public class BM20数组中的逆序对 {
      * @param array
      * @return
      */
-
+    public int mod = 1000000007;
     public int InversePairs(int [] array) {
         int n=array.length;
         //存储逆序对的个数
@@ -31,7 +31,26 @@ public class BM20数组中的逆序对 {
         int mid=left+(right-left)>>1;
         //数组中总的逆序对的个数
         int count=mergeSort(array,ans,left,mid)+mergeSort(array,ans,mid+1,right);
-
+        //防止溢出
+        count %= mod;
+        //划分后的数组起始位置索引
+        int i=left;
+        int j=mid+1;
+        for(int k = left; k <= right; k++)
+            array[k] = ans[k];
+        for(int k = left; k <= right; k++){
+            if(i == mid + 1)
+                array[k] = ans[j++];
+            else if(j == right + 1 || ans[i] <= ans[j])
+                array[k] = ans[i++];
+                //左边比右边大，答案增加
+            else{
+                array[k] = ans[j++];
+                // 统计逆序对
+                count += mid - i + 1;
+            }
+        }
+        return count %mod;
     }
-
 }
+
