8de93d2250b8c61b0832ec47753b10e42c4c41cd
35890 <35890226@qq.com>
2022-07-24 00:11:00 +0800

    新增 BM83字符串变形

--- src/newCoder101/字符串/BM83字符串变形.java
+++ src/newCoder101/字符串/BM83字符串变形.java
@@ -5,33 +5,15 @@ import java.util.Stack;
 public class BM83字符串变形 {
 
     public String trans(String s, int n) {
-        // write code here
-        if (n == 0) {
-            return s;
-        }
-        StringBuilder sb = new StringBuilder(s);
-        for (int i = 0; i < sb.length(); i++) {
-            char c = sb.charAt(i);
-            //大小写转换
-            if (c >= 'A' && c <= 'Z') {
-                sb.append(c - 'A' + 'a');
-            } else if (c >= 'a' && c <= 'z') {
-                sb.append(c - 'a' + 'A');
-            } else {
-                //遇到空格，直接拼接
-                sb.append(c);
+        StringBuilder sb =new StringBuilder();
+         String[] split = s.split(" ",-1);
+        for (int i = split.length-1;i >=0 ; i--) {
+            char[] c =split[i].toCharArray();
+            for (char c1 : c) {
+                sb.append(Character.isLowerCase(c1)?Character.toUpperCase(c1):Character.toLowerCase(c1));
             }
+         sb.append(" ");
         }
-        String newStr =sb.toString();
-        StringBuilder stringBuilder =new StringBuilder();
-        Stack<String> stack =new Stack<>();
-         String[] split = newStr.split(" ");
-        for (String s1 : split) {
-            stack.push(s);
-        }
-        while (!stack.isEmpty()){
-            stringBuilder.append(stack.pop()+" ");
-        }
-        return stringBuilder.toString();
+        return sb.substring(0,sb.length()-1).toString();
     }
     }
