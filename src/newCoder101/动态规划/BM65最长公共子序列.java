38ae6923732e44e750fd7f2cfab27ec781ddea95
35890 <35890226@qq.com>
2022-07-23 23:04:05 +0800

    新增 BM65最长公共子序列

--- src/newCoder101/动态规划/BM65最长公共子序列.java
+++ src/newCoder101/动态规划/BM65最长公共子序列.java
@@ -7,6 +7,7 @@ public class BM65最长公共子序列 {
         int n1=s1.length();
         int n2=s2.length();
         //表示当处理到s1的第i个元素和s2的第j个元素时公共子序列的长度
+        //状态转移方程：前一个字符相等，则dp[i][j] =dp[i-1][j-1]+1;不相等，则dp[i][j]为前一者的最大值
         String[][] dp =new String[n1+1][n2+1];
         for (int i = 0; i <=n1 ; i++) {
             for (int j = 0; j <=n2 ; j++) {
