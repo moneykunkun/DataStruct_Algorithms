3416ac6f91bbd9f0a33cd0b00516c06885d04056
35890 <35890226@qq.com>
2022-07-26 23:21:47 +0800

    新增 BM71最长上升子序列

---
+++ src/newCoder101/动态规划/BM71最长上升子序列.java
@@ -0,0 +1,24 @@
+package newCoder101.动态规划;
+
+public class BM71最长上升子序列 {
+
+    public int LIS (int[] arr) {
+        // write code here
+        // write code here
+        if(arr.length==0) return 0;
+        int[] dp =new int[arr.length];
+        dp[0] =1;
+        //保存最大长度的个数
+        int result =1;
+        for(int i=1;i<arr.length;i++){
+            dp[i]=1;
+            for(int j=0;j<i;j++){
+                if(arr[i]>arr[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+            result =Math.max(result,dp[i]);
+        }
+        return result;
+    }
+}
