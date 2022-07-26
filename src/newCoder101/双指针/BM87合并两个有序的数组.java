154a263e0aeba49032db9b06e3ae9c5c38fabe9b
35890 <35890226@qq.com>
2022-07-26 23:11:37 +0800

    新增 BM87合并两个有序的数组

--- src/newCoder101/双指针/BM87合并两个有序的数组.java
+++ src/newCoder101/双指针/BM87合并两个有序的数组.java
@@ -11,10 +11,12 @@ public class BM87合并两个有序的数组 {
      */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1, j = n - 1, index = m + n - 1;
-        while (i >= 0 && j >= 0)
+        while (i >= 0 && j >= 0) {
             nums1[index--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
-        while (j >= 0)
+        }
+        while (j >= 0) {
             nums1[index--] = nums2[j--];
+        }
     }
 
 
