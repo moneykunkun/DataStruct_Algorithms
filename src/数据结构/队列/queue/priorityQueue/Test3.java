eca1461211b0f98e88ea0ab82c9cfda1a5d72533
35890 <35890226@qq.com>
2022-07-28 00:51:18 +0800

    新增 优先级队列

--- src/数据结构/队列/queue/priorityQueue/Test3.java
+++ src/数据结构/队列/queue/priorityQueue/Test3.java
@@ -50,12 +50,22 @@ public class Test3 {
         Student s1 =new Student("胡歌",30);
         Student s2 =new Student("江江",24);
         Student s3 =new Student("浩存",20);
+        //1.比较器写法
         Queue<Student> queue =new PriorityQueue<>(new StudentDesc());
-        queue.offer(s1);
-        queue.offer(s2);
-        queue.offer(s3);
-        while (!queue.isEmpty()){
-            System.out.println(queue.poll());
+        //2.匿名内部类写法
+        Queue<Student> queue1 =new PriorityQueue<>(new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return o2.age-o1.getAge();
+            }
+        });
+        //3.lambda表达式
+        Queue<Student> queue2 =new PriorityQueue<>(((o1, o2) -> o2.age- o1.age));
+        queue2.offer(s1);
+        queue2.offer(s2);
+        queue2.offer(s3);
+        while (!queue2.isEmpty()){
+            System.out.println(queue2.poll());
         }
     }
 }
