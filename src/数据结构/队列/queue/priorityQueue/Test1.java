fa8cf5b1d0141639d31827cbcb5a4d99f1772d75
35890 <35890226@qq.com>
2022-07-28 00:36:17 +0800

    新增 Comparator接口

--- src/数据结构/队列/queue/priorityQueue/Test1.java
+++ src/数据结构/队列/queue/priorityQueue/Test1.java
@@ -29,7 +29,7 @@ public class Test1 {
     public static void main(String[] args) {
         Student s1 =new Student("胡歌",30);
         Student s2 =new Student("江江",24);
-        Student s3 =new Student("浩存",00);
+        Student s3 =new Student("浩存",20);
         Student[] students =new Student[]{s1,s2,s3};
         Arrays.sort(students);
         System.out.println(Arrays.toString(students));
