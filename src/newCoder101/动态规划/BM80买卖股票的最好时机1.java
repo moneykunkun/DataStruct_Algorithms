774132f4ce67715d5271ea68448948b3013a8c76
35890 <35890226@qq.com>
2022-08-08 23:34:05 +0800

    新增 BM80买卖股票的最好时机1

--- src/newCoder101/动态规划/BM80买卖股票的最好时机1.java
+++ src/newCoder101/动态规划/BM80买卖股票的最好时机1.java
@@ -22,4 +22,27 @@ public class BM80买卖股票的最好时机1 {
         }
         return maxProfit;
     }
-}
+
+    /**
+     * 动态规划
+     * 性能较差
+     * @param prices
+     * @return
+     */
+        public int maxProfit1(int[] prices) {
+            int length = prices.length;
+            //初始化
+            int[][] dp = new int[length][2];
+            //base case：第一天的时候，没股票手里利润是0，有股票的时候手里利润是-prices[0]
+            dp[0][1] = -prices[0];
+            dp[0][0] = 0;
+            for(int i = 1;i < length; i ++){
+                //手里没股票：
+                dp[i][0] = Math.max(dp[i - 1][0],dp[i - 1][1] + prices[i]);
+                //手里有股票
+                dp[i][1] = Math.max(dp[i - 1][1],- prices[i]);
+            }
+            return dp[length - 1][0];
+        }
+    }
+
