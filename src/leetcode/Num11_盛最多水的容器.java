6f6f8f587f6d4742e1786499c082aa52eedd7d0f
35890 <35890226@qq.com>
2022-08-24 23:24:11 +0800

    新增 leetcode练习-Num5_最长回文子串

---
+++ src/leetcode/Num11_盛最多水的容器.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class Num11_盛最多水的容器 {
+    public int maxArea(int[] height) {
+        int n =height.length;
+        if(n<2){
+            return 0;
+        }
+        int ans =0;
+        int left =0;
+        int right =n-1;
+        while(left <right){
+            int capacity =Math.min(height[left],height[right])*(right-left);
+            ans =Math.max(capacity,ans);
+            if(height[left] <height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return ans;
+    }
+}
