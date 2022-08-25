da79cf3ff0ed797cf8261d57383d01782c9de765
35890 <35890226@qq.com>
2022-08-26 00:09:53 +0800

    新增 leetcode练习-Num239_滑动窗口最大值

---
+++ src/leetcode/Num239_滑动窗口最大值.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Num239_滑动窗口最大值 {
+
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            int n = nums.length;
+            Deque<Integer> deque = new LinkedList<Integer>();
+            for (int i = 0; i < k; ++i) {
+                while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                    deque.pollLast();
+                }
+                deque.offerLast(i);
+            }
+
+            int[] ans = new int[n - k + 1];
+            ans[0] = nums[deque.peekFirst()];
+            for (int i = k; i < n; ++i) {
+                while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                    deque.pollLast();
+                }
+                deque.offerLast(i);
+                while (deque.peekFirst() <= i - k) {
+                    deque.pollFirst();
+                }
+                ans[i - k + 1] = nums[deque.peekFirst()];
+            }
+            return ans;
+        }
+    }
+
