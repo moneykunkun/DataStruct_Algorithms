7a32ecbd7e81821ff3b0ebd627ad46d1d6786232
35890 <35890226@qq.com>
2022-08-25 23:36:39 +0800

    新增 leetcode练习-Num128_最长连续序列 排序

--- src/leetcode/Num128_最长连续序列.java
+++ src/leetcode/Num128_最长连续序列.java
@@ -17,11 +17,21 @@ public class Num128_最长连续序列 {
         Arrays.sort(nums);
         //记录连续最长数组的长度
         int[] res =new int[nums.length];
+        res[0] =1;
         for (int i = 1; i <nums.length ; i++) {
             if (nums[i] ==nums[i-1]+1){
-                //连续的情况 在上
+                //连续的情况 长度在上一个基础上+1
                 res[i] =res[i-1]+1;
+            }else if (nums[i] ==nums[i-1]){
+                //前后相等
+                res[i] =res[i-1];
+            }else {
+                //否则，连续长度就为1
+                res[i] =1;
             }
         }
+        //对保存长度记录排序，返回最长的记录
+        Arrays.sort(res);
+        return res[res.length-1];
     }
 }
