7f90e37c8273092aa57d7dcc2397a3c98826b769
35890 <35890226@qq.com>
2022-08-25 23:06:44 +0800

    新增 leetcode练习-Num39_组合总和 深度优先遍历

--- src/leetcode/Num39_组合总和.java
+++ src/leetcode/Num39_组合总和.java
@@ -16,13 +16,32 @@ public class Num39_组合总和 {
 
     /**
      * @param candidates    候选数组
-     * @param i
+     * @param begin 搜索起始位置
      * @param len
-     * @param target
-     * @param path
-     * @param res
+     * @param target    目标值，没减去一个元素，目标值减小
+     * @param path  存储根节点到叶子节点的路径，栈
+     * @param res   存储结果集
      */
-    private void dfs(int[] candidates, int i, int len, int target, Deque<Integer> path, List<List<Integer>> res) {
+    private void dfs(int[] candidates, int begin, int len, int target, Deque<Integer> path, List<List<Integer>> res) {
+       //目标值小于0，不存在
+        if (target< 0){
+            return;
+        }
+        if (target ==0){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        //开始深度优先搜索
+        for (int i = begin; i <len ; i++) {
+            //每表里一个元素 就加入到路径中
+            path.addLast(candidates[i]);
+
+            //元素可以重复使用，因此下次开始遍历的时候仍然从i开始
+            dfs(candidates,i,len,target-candidates[i],path,res);
+            //状态重置
+            //不包含重复项，每次遍历后需要剪枝
+            path.removeLast();
+        }
 
     }
 }
