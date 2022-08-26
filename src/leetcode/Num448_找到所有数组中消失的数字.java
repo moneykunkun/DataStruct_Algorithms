6cd656efcbef0551baba81c11d06e0df44c7f75c
35890 <35890226@qq.com>
2022-08-26 12:28:59 +0800

    新增 leetcode练习-Num448_找到所有数组中消失的数字

---
+++ src/leetcode/Num448_找到所有数组中消失的数字.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Num448_找到所有数组中消失的数字 {
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length;
+        for (int num : nums) {
+            int x = (num - 1) % n;
+            nums[x] += n;
+        }
+        List<Integer> ret = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= n) {
+                ret.add(i + 1);
+            }
+        }
+        return ret;
+    }
+}
