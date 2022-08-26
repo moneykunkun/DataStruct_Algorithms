43628a3902ffe05cad2c6af497cd2a00db5e2705
35890 <35890226@qq.com>
2022-08-26 12:23:00 +0800

    新增 leetcode练习-岛屿数量 DFS

--- src/leetcode/Num200_岛屿数量.java
+++ src/leetcode/Num200_岛屿数量.java
@@ -9,6 +9,7 @@ public class Num200_岛屿数量 {
      * @return
      */
         public int numIslands(char[][] grid) {
+            //记录岛屿的数量
             int count = 0;
             for(int i = 0; i < grid.length; i++) {
                 for(int j = 0; j < grid[0].length; j++) {
@@ -21,8 +22,12 @@ public class Num200_岛屿数量 {
             return count;
         }
         private void dfs(char[][] grid, int i, int j){
-            if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') return;
+            //grid[i][j] 不在方格内直接退出
+            if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
+                return;
+            }
             grid[i][j] = '0';
+            //从四个方向深度遍历
             dfs(grid, i + 1, j);
             dfs(grid, i, j + 1);
             dfs(grid, i - 1, j);
