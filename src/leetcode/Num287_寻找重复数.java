43169877a7c1c93a0c27a0b05a6345bd3c609d0d
35890 <35890226@qq.com>
2022-08-28 23:10:01 +0800

    新增 leetcode练习- Num287_寻找重复数--二分查找

--- src/leetcode/Num287_寻找重复数.java
+++ src/leetcode/Num287_寻找重复数.java
@@ -18,4 +18,30 @@ public class Num287_寻找重复数 {
         }
         return num;
     }
+
+    /**
+     * 二分查找
+     * @param nums
+     * @return
+     */
+    public int findDuplicate1(int[] nums) {
+        int n = nums.length;
+        int l = 1, r = n - 1, ans = -1;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            int cnt = 0;
+            for (int i = 0; i < n; ++i) {
+                if (nums[i] <= mid) {
+                    cnt++;
+                }
+            }
+            if (cnt <= mid) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
 }
