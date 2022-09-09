21ee761319427108ed3b35f7da12a101b236f51c
35890 <35890226@qq.com>
2022-09-09 16:24:06 +0800

    新增

---
+++ src/面试/Tencent.java
@@ -0,0 +1,19 @@
+package 面试;
+
+import java.util.HashMap;
+
+public class Tencent {
+
+    public int minOperations (String str) {
+        // write code here
+        int len =str.length();
+        HashMap<Character,Integer> map =new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c =str.charAt(i);
+            //统计每个字符出现的次数
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+
+    }
+}
