a563febd8c3271c9d9906e6533f565ebeb59a7c8
35890 <35890226@qq.com>
2022-09-12 20:14:43 +0800

    新增 数组中重复的数字

---
+++ src/剑指offer/Num03_数组中重复的数字.java
@@ -0,0 +1,39 @@
+package 剑指offer;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Num03_数组中重复的数字 {
+
+    public int findRepeatNumber(int[] nums) {
+        HashMap<Integer,Integer> map =new HashMap<>();
+        for (int num : nums) {
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        int res =-1;
+        for (Map.Entry<Integer,Integer> entry:map.entrySet()){
+            if (entry.getValue()>1){
+             res =entry.getKey();
+            }
+        }
+        return  res;
+    }
+
+    /**
+     * set集合
+     * @param nums
+     * @return
+     */
+    public int findRepeatNumber1(int[] nums) {
+        int res =-1;
+        HashSet<Integer> set =new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)){
+                res =num;
+            }
+        }
+        return res;
+    }
+
+}
