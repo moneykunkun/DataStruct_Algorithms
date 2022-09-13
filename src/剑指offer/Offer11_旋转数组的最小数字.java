36ee2748d05bbd381287923e3460f99a1b27fd05
35890 <35890226@qq.com>
2022-09-13 11:40:47 +0800

    新增 Offer11_旋转数组的最小数字

---
+++ src/剑指offer/Offer11_旋转数组的最小数字.java
@@ -0,0 +1,22 @@
+package 剑指offer;
+
+public class Offer11_旋转数组的最小数字 {
+    //二分查找
+    public int minArray(int[] numbers) {
+        int left =0;
+        int right =numbers.length-1;
+        while (left<right){
+            int mid =left+(right-left)/2;
+            if (numbers[mid] >numbers[right]){
+                //左区间已经有序
+                left=mid+1;
+            }else if (numbers[mid] <numbers[right]){
+                //右区间已经有序
+                right =mid;
+            }else if (numbers[mid] ==numbers[right]){
+                right--;
+            }
+        }
+        return numbers[left];
+    }
+}
