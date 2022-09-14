22fbc60778e87ac12eeb0b6a8ef4bf6aebb7dd19
35890 <35890226@qq.com>
2022-09-14 13:00:51 +0800

    新增 Offer15_二进制中1的个数 位运算

---
+++ src/剑指offer/Offer15_二进制中1的个数.java
@@ -0,0 +1,15 @@
+package 剑指offer;
+
+public class Offer15_二进制中1的个数 {
+
+    public int hammingWeight(int n) {
+        int res =0;
+        while (n!=0){
+            //n&1 =1 ,表示n最右以为为1，n&1 =0,表示n最右侧一位为0
+            res +=n&1;
+            //无符号右移一位
+            n>>>=1;
+        }
+        return res;
+    }
+}
