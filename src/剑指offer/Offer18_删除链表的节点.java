6bceb8a7a062273a7f6061d9b4b7babc7e4636d6
35890 <35890226@qq.com>
2022-09-14 15:07:12 +0800

    新增 Offer16_数值的整数次方

--- src/剑指offer/Offer18_删除链表的节点.java
+++ src/剑指offer/Offer18_删除链表的节点.java
@@ -23,4 +23,27 @@ public class Offer18_删除链表的节点 {
         }
         return head;
     }
+
+    /**
+     * 双指针解法
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode deleteNode2(ListNode head, int val) {
+            if (head.val ==val){
+                return head.next;
+            }
+            ListNode prev =head;            //待删除节点的前驱节点
+            ListNode cur =head.next;
+            while (cur !=null && cur.val !=val){
+                prev =cur;
+                cur =cur.next;
+            }
+            //此时，prev就是待删除节点的前驱节点删除节点
+            if (cur !=null){
+                prev.next =cur.next;
+            }
+            return head;
+    }
 }
