a8a8d64809a3a9df24314bedaf9740a37d365412
35890 <35890226@qq.com>
2022-09-20 14:58:58 +0800

    贪心算法 盛水最多的容器

---
+++ src/算法/贪心/Lc11_盛最多水的容器.java
@@ -0,0 +1,26 @@
+package 算法.贪心;
+
+public class Lc11_盛最多水的容器 {
+
+    //双指针
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length-1;
+        //记录最大值
+        int ans = 0;
+        while (left < right) {
+            //最大面积 = 左右边界的较小者 * 边界距离的差值
+        int maxArea = Math.min(height[left],height[right]) * (right-left);
+            //更新最大值
+        ans = Math.max(ans,maxArea);
+        if (height[left] <= height[right]){
+           //左指针移动
+            ++left;
+        }else {
+            //右指针移动
+            ++right;
+        }
+        }
+        return ans;
+    }
+
+}
