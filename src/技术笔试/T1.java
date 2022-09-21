9a7808851d27861ad6dd4d68d1dedd2d473aeb58
35890 <35890226@qq.com>
2022-09-21 22:21:15 +0800

    新增 笔试

---
+++ src/技术笔试/T1.java
@@ -0,0 +1,13 @@
+package 技术笔试;
+
+
+public class T1 {
+    public static void main(String[] args) {
+        int[] arr ={1,2,3,4,5,6,7,8};
+
+    }
+    public static int[] splitArr(int[] arr) {
+       int index =0;
+
+    }
+}
