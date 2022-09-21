e23024649081bb9250d98fcfdf423e290256760d
35890 <35890226@qq.com>
2022-09-21 22:20:52 +0800

    新增 笔试

---
+++ src/技术笔试/B_回文矩阵.java
@@ -0,0 +1,25 @@
+package 技术笔试;
+import java.util.Scanner;
+
+public class B_回文矩阵 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] arr =new int[m][n];
+        int[] s =new int[10];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        long res =0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+            }
+        }
+
+    }
+
+}
