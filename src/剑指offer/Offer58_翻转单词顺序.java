2cf39e9163f5020f61df483912a3ddc0d81f8158
35890 <35890226@qq.com>
2022-09-25 22:06:03 +0800

    新增 Offer58_翻转单词顺序

---
+++ src/剑指offer/Offer58_翻转单词顺序.java
@@ -0,0 +1,23 @@
+package 剑指offer;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Offer58_翻转单词顺序 {
+
+    public String reverseWords(String s) {
+        //去除首尾的空格后按空格拆分
+        String[] str = s.trim().split(" ");
+        StringBuilder sb =new StringBuilder();
+        //倒序遍历数组
+        for (int i = str.length-1; i >=0 ; i--) {
+            if (str[i].equals("")){         //遇到空单词就跳过
+                continue;
+            }else {
+                sb.append(str[i]).append(" ");
+            }
+        }
+
+        return sb.toString().trim();
+    }
+}
