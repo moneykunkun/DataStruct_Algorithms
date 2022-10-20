35433b0d53371a0fa570d2c92a3e8e10425df583
qk <qk35890226@163.com>
2022-10-20 10:00:39 +0800

    最长无重复字串

--- src/leetcode/Num3_无重复字符的最长子串.java
+++ src/leetcode/Num3_无重复字符的最长子串.java
@@ -26,4 +26,33 @@ public class Num3_无重复字符的最长子串 {
         }
         return ans;
     }
+
+    /**
+     * 滑动窗口的解题思路
+     */
+
+    public int lengthOfLongestSubstring2(String s) {
+        //记录最大值的变量
+        int maxLen =0;
+        //定义两个指针
+        int left =0;
+        int right =0;
+        //保存字符的集合
+        Set<Character> set =new HashSet<>();
+        while (right<s.length()){
+            //如果窗口中包含当前元素，我们再继续添加就会出现重复值
+            //所以在添加前，先将窗口最左端的元素移除掉，直到窗口不包含当前元素为止
+            while (set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));         //移除窗口左边元素
+                left++;             //左指针右移
+            }
+            //添加当前元素到集合中
+            set.add(s.charAt(right));
+            //更新最大值
+            maxLen =Math.max(maxLen,set.size());
+            //右指针移动
+            right++;
+        }
+        return maxLen;
+    }
 }
