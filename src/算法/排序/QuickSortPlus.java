9b2f55442126d84d081c4c551b256856fd33a9ea
qk <qk35890226@163.com>
2023-04-26 10:43:59 +0800

    Default Changelist

--- src/算法/排序/QuickSortPlus.java
+++ src/算法/排序/QuickSortPlus.java
@@ -8,63 +8,49 @@ import java.util.Arrays;
 public class QuickSortPlus {
     /**
      * 快速排序
-     * 时间复杂度 NlogN
+     * 时间复杂度：O(NlogN)~O(N^2) 平均值为：NlogN
+     * 空间复杂度：O(logN)
+     * 不稳定的排序算法
      * @param arr
      */
-    public static void quickSort(int[] arr,int left,int right){
-        //找到基准值
-        if (left>=right) {
-            return;
+    public static void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            // 划分操作
+            int pivot = partition(arr, left, right);
+            // 递归排序左右子序列
+            quickSort(arr, left, pivot - 1);
+            quickSort(arr, pivot + 1, right);
         }
-        //找到基准值
-        int pivot=partition(arr,left,right);
-
-        //递归基准值的左区间
-        quickSort(arr,left,pivot-1);
-        //递归基准值的右区间
-        quickSort(arr,pivot+1,right);
     }
 
-    /**
-     * 分区函数，找到基准值
-     * @param arr
-     * @param left
-     * @param right
-     * @return
-     */
-    public static int partition(int[] arr,int left,int right){
-        //1.找临时基准值
-        int tmp =arr[left];
-
-        while (left<right){
-            //从后往前看，和基准值比较
-            while (left <right &&arr[right] >=tmp){
-                right--;
+    public static int partition(int[] arr, int left, int right) {
+        // 选择第一个元素作为枢轴元素
+        int pivot = arr[left];
+        int i = left, j = right;
+        while (i < j) {
+            while (i < j && arr[j] >= pivot) {
+                j--;
             }
-            //两指针相遇
-            if (left>=right){
-                arr[left] =tmp;
-                break;
-            }else {
-                arr[left] =arr[right];
+            if (i < j) {
+                arr[i++] = arr[j];
             }
-            while (left <right &&arr[left] <=tmp){
-                left++;
+            while (i < j && arr[i] < pivot) {
+                i++;
             }
-            if (left>=right){
-                arr[left] =tmp;
-                break;
-            }else {
-                arr[right] =arr[left];
+            if (i < j) {
+                arr[j--] = arr[i];
             }
         }
-        return tmp;
+        arr[i] = pivot;
+        return i;
     }
-
 
     public static void main(String[] args) {
-        int[] arr ={1,12,3,7,2,5,10,16};
-        quickSort(arr,0,arr.length-1);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {64, 25, 12, 22, 11};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.print("排序后的数组：");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
 }
